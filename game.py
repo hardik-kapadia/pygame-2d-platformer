68bb611d636b2e88a00525ba7574dafd9eda765f
Romil Shah <romil28@somaiya.edu>
2021-11-26 00:53:18 +0530

    added players

--- game.py
+++ game.py
@@ -1,60 +1,143 @@
 import pygame
-
 from pygame.locals import *
 
 pygame.init()
 
+clock = pygame.time.Clock()
+fps = 60
+
 screen_width = 800
 screen_height = 800
 
-screen = pygame.display.set_mode((screen_width,screen_height))
-pygame.display.set_caption("2D Platformer")
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Platformer')
 
 #define game variables
 tile_size = 40
 
+
 #load images
 sun_img = pygame.image.load('img/sun.png')
 bg_img = pygame.image.load('img/sky.png')
-bg_img = pygame.transform.scale(bg_img,(screen_width,screen_height))
 
-def draw_grid():
-    for line in range(0, 20):
-        pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (screen_width, line * tile_size))
-        pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, screen_height))
+
+class Player():
+	def __init__(self, x, y):
+		self.images_right = []
+		self.images_left = []
+		self.index = 0
+		self.counter = 0
+		for num in range(1, 5):
+			img_right = pygame.image.load(f'img/guy{num}.png')
+			img_right = pygame.transform.scale(img_right, (40, 80))
+			img_left = pygame.transform.flip(img_right, True, False)
+			self.images_right.append(img_right)
+			self.images_left.append(img_left)
+		self.image = self.images_right[self.index]
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+		self.vel_y = 0
+		self.jumped = False
+		self.direction = 0
+
+	def update(self):
+		dx = 0
+		dy = 0
+		walk_cooldown = 5
+
+		#get keypresses
+		key = pygame.key.get_pressed()
+		if key[pygame.K_SPACE] and self.jumped == False:
+			self.vel_y = -15
+			self.jumped = True
+		if key[pygame.K_SPACE] == False:
+			self.jumped = False
+		if key[pygame.K_LEFT]:
+			dx -= 5
+			self.counter += 1
+			self.direction = -1
+		if key[pygame.K_RIGHT]:
+			dx += 5
+			self.counter += 1
+			self.direction = 1
+		if key[pygame.K_LEFT] == False and key[pygame.K_RIGHT] == False:
+			self.counter = 0
+			self.index = 0
+			if self.direction == 1:
+				self.image = self.images_right[self.index]
+			if self.direction == -1:
+				self.image = self.images_left[self.index]
+
+
+		#handle animation
+		if self.counter > walk_cooldown:
+			self.counter = 0	
+			self.index += 1
+			if self.index >= len(self.images_right):
+				self.index = 0
+			if self.direction == 1:
+				self.image = self.images_right[self.index]
+			if self.direction == -1:
+				self.image = self.images_left[self.index]
+
+
+		#add gravity
+		self.vel_y += 1
+		if self.vel_y > 10:
+			self.vel_y = 10
+		dy += self.vel_y
+
+		#check for collision
+
+		#update player coordinates
+		self.rect.x += dx
+		self.rect.y += dy
+
+		if self.rect.bottom > screen_height:
+			self.rect.bottom = screen_height
+			dy = 0
+
+		#draw player onto screen
+		screen.blit(self.image, self.rect)
+
+
+
 
 class World():
-    def __init__(self, data):
-        self.tile_list = []
+	def __init__(self, data):
+		self.tile_list = []
 
-        #load images
-        dirt_img = pygame.image.load('img/dirt.png')
-        grass_img = pygame.image.load('img/grass.png')
+		#load images
+		dirt_img = pygame.image.load('img/dirt.png')
+		grass_img = pygame.image.load('img/grass.png')
 
-        row_count = 0
-        for row in data:
-            col_count = 0
-            for tile in row:
-                if tile == 1:
-                    img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
-                    img_rect = img.get_rect()
-                    img_rect.x = col_count * tile_size
-                    img_rect.y = row_count * tile_size
-                    tile = (img, img_rect)
-                    self.tile_list.append(tile)
-                if tile == 2:
-                    img = pygame.transform.scale(grass_img, (tile_size, tile_size))
-                    img_rect = img.get_rect()
-                    img_rect.x = col_count * tile_size
-                    img_rect.y = row_count * tile_size
-                    tile = (img, img_rect)
-                    self.tile_list.append(tile)
-                col_count += 1
-            row_count += 1
+		row_count = 0
+		for row in data:
+			col_count = 0
+			for tile in row:
+				if tile == 1:
+					img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
+					img_rect = img.get_rect()
+					img_rect.x = col_count * tile_size
+					img_rect.y = row_count * tile_size
+					tile = (img, img_rect)
+					self.tile_list.append(tile)
+				if tile == 2:
+					img = pygame.transform.scale(grass_img, (tile_size, tile_size))
+					img_rect = img.get_rect()
+					img_rect.x = col_count * tile_size
+					img_rect.y = row_count * tile_size
+					tile = (img, img_rect)
+					self.tile_list.append(tile)
+				col_count += 1
+			row_count += 1
 
-    def draw(self):
-        for tile in self.tile_list:
-            screen.blit(tile[0], tile[1])
+	def draw(self):
+		for tile in self.tile_list:
+			screen.blit(tile[0], tile[1])
+
+
 
 world_data = [
 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
@@ -79,25 +162,27 @@ world_data = [
 [1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
-world = World(world_data)
 
-print(len(world_data))
-print(len(world_data[0]))
+
+player = Player(100, screen_height - 130)
+world = World(world_data)
 
 run = True
 while run:
 
-    screen.blit(bg_img, (0, 0))
-    screen.blit(sun_img, (80, 80))
+	clock.tick(fps)
 
-    world.draw()
+	screen.blit(bg_img, (0, 0))
+	screen.blit(sun_img, (100, 100))
 
-    # draw_grid()
+	world.draw()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+	player.update()
 
-    pygame.display.update()
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			run = False
+
+	pygame.display.update()
 
 pygame.quit()
