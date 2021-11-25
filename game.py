f35bf7d0c580854d7179f98ad0fc669e09bbaa79
Romil Shah <romil28@somaiya.edu>
2021-11-26 02:45:25 +0530

    added scoring system

--- game.py
+++ game.py
@@ -1,8 +1,11 @@
 import pygame
 from pygame.locals import *
+from pygame import mixer
 import pickle
 from os import path
 
+pygame.mixer.pre_init(44100, -16, 2, 512)
+mixer.init()
 pygame.init()
 
 clock = pygame.time.Clock()
@@ -14,12 +17,24 @@ screen_height = 800
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Platformer')
 
+
+#define font
+font = pygame.font.SysFont('Bauhaus 93', 70)
+font_score = pygame.font.SysFont('Bauhaus 93', 30)
+
+
 #define game variables
 tile_size = 40
 game_over = 0
 main_menu = True
 level = 0
 max_levels = 7
+score = 0
+
+
+#define colours
+white = (255, 255, 255)
+blue = (0, 0, 255)
 
 
 #load images
@@ -28,6 +43,21 @@ bg_img = pygame.image.load('img/sky.png')
 restart_img = pygame.image.load('img/restart_btn.png')
 start_img = pygame.image.load('img/start_btn.png')
 exit_img = pygame.image.load('img/exit_btn.png')
+
+#load sounds
+pygame.mixer.music.load('img/music.wav')
+pygame.mixer.music.play(-1, 0.0, 5000)
+coin_fx = pygame.mixer.Sound('img/coin.wav')
+coin_fx.set_volume(0.5)
+jump_fx = pygame.mixer.Sound('img/jump.wav')
+jump_fx.set_volume(0.5)
+game_over_fx = pygame.mixer.Sound('img/game_over.wav')
+game_over_fx.set_volume(0.5)
+
+
+def draw_text(text, font, text_col, x, y):
+	img = font.render(text, True, text_col)
+	screen.blit(img, (x, y))
 
 
 #function to reset level
@@ -91,6 +121,7 @@ class Player():
 			#get keypresses
 			key = pygame.key.get_pressed()
 			if key[pygame.K_SPACE] and self.jumped == False and self.in_air == False:
+				jump_fx.play()
 				self.vel_y = -15
 				self.jumped = True
 			if key[pygame.K_SPACE] == False:
@@ -152,10 +183,12 @@ class Player():
 			#check for collision with enemies
 			if pygame.sprite.spritecollide(self, blob_group, False):
 				game_over = -1
+				game_over_fx.play()
 
 			#check for collision with lava
 			if pygame.sprite.spritecollide(self, lava_group, False):
 				game_over = -1
+				game_over_fx.play()
 
 			#check for collision with exit
 			if pygame.sprite.spritecollide(self, exit_group, False):
@@ -169,6 +202,7 @@ class Player():
 
 		elif game_over == -1:
 			self.image = self.dead_image
+			draw_text('GAME OVER!', font, blue, (screen_width // 2) - 200, screen_height // 2)
 			if self.rect.y > 200:
 				self.rect.y -= 5
 
@@ -236,6 +270,9 @@ class World():
 				if tile == 6:
 					lava = Lava(col_count * tile_size, row_count * tile_size + (tile_size // 2))
 					lava_group.add(lava)
+				if tile == 7:
+					coin = Coin(col_count * tile_size + (tile_size // 2), row_count * tile_size + (tile_size // 2))
+					coin_group.add(coin)
 				if tile == 8:
 					exit = Exit(col_count * tile_size, row_count * tile_size - (tile_size // 2))
 					exit_group.add(exit)
@@ -278,6 +315,15 @@ class Lava(pygame.sprite.Sprite):
 		self.rect.y = y
 
 
+class Coin(pygame.sprite.Sprite):
+	def __init__(self, x, y):
+		pygame.sprite.Sprite.__init__(self)
+		img = pygame.image.load('img/coin.png')
+		self.image = pygame.transform.scale(img, (tile_size // 2, tile_size // 2))
+		self.rect = self.image.get_rect()
+		self.rect.center = (x, y)
+
+
 class Exit(pygame.sprite.Sprite):
 	def __init__(self, x, y):
 		pygame.sprite.Sprite.__init__(self)
@@ -293,8 +339,12 @@ player = Player(100, screen_height - 130)
 
 blob_group = pygame.sprite.Group()
 lava_group = pygame.sprite.Group()
+coin_group = pygame.sprite.Group()
 exit_group = pygame.sprite.Group()
 
+#create dummy coin for showing the score
+score_coin = Coin(tile_size // 2, tile_size // 2)
+coin_group.add(score_coin)
 
 #load in level data and create world
 if path.exists(f'levels/level{level}_data'):
@@ -327,9 +377,16 @@ while run:
 
 		if game_over == 0:
 			blob_group.update()
+			#update score
+			#check if a coin has been collected
+			if pygame.sprite.spritecollide(player, coin_group, True):
+				score += 1
+				coin_fx.play()
+			draw_text('X ' + str(score), font_score, white, tile_size - 10, 10)
 		
 		blob_group.draw(screen)
 		lava_group.draw(screen)
+		coin_group.draw(screen)
 		exit_group.draw(screen)
 
 		game_over = player.update(game_over)
@@ -340,6 +397,7 @@ while run:
 				world_data = []
 				world = reset_level(level)
 				game_over = 0
+				score = 0
 
 		#if player has completed the level
 		if game_over == 1:
@@ -351,12 +409,14 @@ while run:
 				world = reset_level(level)
 				game_over = 0
 			else:
+				draw_text('YOU WIN!', font, blue, (screen_width // 2) - 140, screen_height // 2)
 				if restart_button.draw():
 					level = 1
 					#reset level
 					world_data = []
 					world = reset_level(level)
 					game_over = 0
+					score = 0
 
 
 
